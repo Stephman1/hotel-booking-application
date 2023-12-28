1fe8f834f31007989b1c89eb07506222f0cd7ed5
Stephman1 <keelers@seas.upenn.edu>
2023-12-28 18:09:57 +0000

    Trying to use Spring Boot for API

--- hotelbooking/src/main/java/group_id/hotelbooking/App.java
+++ hotelbooking/src/main/java/group_id/hotelbooking/App.java
@@ -1,6 +1,8 @@
 package group_id.hotelbooking;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
