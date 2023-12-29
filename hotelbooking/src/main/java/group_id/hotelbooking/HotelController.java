61259f77456b0f9aabdc2dbe2c0d4a32fdd7f0b7
Stephman1 <keelers@seas.upenn.edu>
2023-12-29 01:19:40 +0000

    Working but still a bit random

--- hotelbooking/src/main/java/group_id/hotelbooking/HotelController.java
+++ hotelbooking/src/main/java/group_id/hotelbooking/HotelController.java
@@ -11,9 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
--- hotelbooking/src/main/java/group_id/hotelbooking/HotelRoom.java
+++ hotelbooking/src/main/java/group_id/hotelbooking/HotelRoom.java
@@ -1,6 +1,5 @@
 package group_id.hotelbooking;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,71 +9,39 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "hotel_rooms")
-@Data
-//@Getter
-//@Setter
-//@ToString
-//@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class HotelRoom {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    //@Column(name = "room_number")
+    @Column(name = "room_number")
     private Integer roomNumber;
 
-    //@Column(name = "number_of_people")
+    @Column(name = "number_of_people")
     private Integer numberOfPeople;
     
-    //@Column(name = "price")
+    @Column(name = "price")
     private Integer price;
     
-    //@Column(name = "room_type")
+    @Column(name = "room_type")
     private String roomType;
 
-    //@Column(name = "is_occupied")
+    @Column(name = "is_occupied")
     private Boolean isOccupied;
     
-    //@Column(name = "hotel")
+    @Column(name = "hotel")
     private String hotel;
     
-    /**
     public HotelRoom() {
     	// Default constructor
     }
     
-    
     // Methods
     
-    @Override
-    public String toString() {
-        return "HotelRoom{" +
-                "id=" + id +
-                ", roomNumber=" + roomNumber +
-                ", numberOfPeople=" + numberOfPeople +
-                ", price=" + price +
-                ", roomType='" + roomType + '\'' +
-                ", isOccupied=" + isOccupied +
-                ", hotel='" + hotel + '\'' +
-                '}';
-    }
-	
-	public Integer getPrice() {
-		return price;
-	}
-
-	public Integer getRoomNumber() {
-		return roomNumber;
-	}
-
-	public String getHotel() {
-		return hotel;
-	}
-	
-	public String getRoomType() {
-		return roomType;
-	}
-	
 	public void setIsOccupied(Boolean occupied) {
 		isOccupied = occupied;
 	}
@@ -82,6 +49,5 @@ public class HotelRoom {
 	public void setPrice(Integer newPrice) {
 		price = newPrice;
 	}
-	*/
 
 }
